3b37d710c288bef6726491046c3659e89002f65e
IsaacAllen <iallen@students.inghamisd.org>
2016-03-31 08:52:24 -0400

    el

--- SpaceGame/Sprite.cs
+++ SpaceGame/Sprite.cs
@@ -16,27 +16,27 @@ namespace SpaceGame
         public string AssetName;
         private Texture2D baseShip, Starrybackground, Wall;
         // Size of sprite
-        public Rectangle Size;
+        public Rectangle Size, shipBoundingBox, wallBoundingBox;
         // Amount to increase/decrease size
         private float mScale = 1.0f;
         //For Updating under move
         public Vector2 Position = Vector2.Zero;
 
-        public Rectangle BoundingBox
+        public Rectangle ShipBoundingBox
         {
             get
             {
-                return new Rectangle((int)Position.X, (int)Position.Y, baseShip.Width, baseShip.Height);
+                return shipBoundingBox;
             }
         }
 
-        //public Rectangle BoundingBox
-        //{
-           // get
-           // {
-           //     return new Rectangle((int)Position.X, (int)Position.Y, Wall.Width, Wall.Height);
-           // }
-       // }
+        public Rectangle WallBoundingBox
+        {
+            get
+           {
+               return wallBoundingBox;
+            }
+        }
 
         public float Scale
         {
@@ -50,12 +50,15 @@ namespace SpaceGame
         public virtual void LoadContent(ContentManager cM) { }
         public virtual void LoadContent(ContentManager theContentManager, string theAssetName)
         {
+            baseShip = theContentManager.Load<Texture2D>("Sprites/Base Ship");
             Wall = theContentManager.Load<Texture2D>("Sprites/Wall");
             Starrybackground = theContentManager.Load<Texture2D>("Sprites/maxresdefault");
         }
         public virtual void Update(GameTime gameTime, Vector2 theSpeed, Vector2 theDirection)
         {
             Position += theDirection * theSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            shipBoundingBox = new Rectangle((int)Position.X, (int)Position.Y, baseShip.Width, baseShip.Height);
+            wallBoundingBox = new Rectangle((int)Position.X, (int)Position.Y, Wall.Width, Wall.Height);
         }
         public virtual void Draw(SpriteBatch spriteBatch)
         {
